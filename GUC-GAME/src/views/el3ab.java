716601ef27f157dc2882dd15bcbb4273f1dd8493
Abdelrahman <abodykhalil65@gmail.com>
2023-05-25 21:31:44 +0300

    el3ab yala

--- GUC-GAME/src/views/el3ab.java
+++ GUC-GAME/src/views/el3ab.java
@@ -1,91 +1,76 @@
 package views;
-
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import jaco.mp3.a.s;
 import javafx.application.*;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
-import model.characters.Hero;
+//import model.characters.Hero;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Button;
 import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.scene.text.*;
+import javafx.scene.paint.Color;
 
-public class el3ab extends Application{
-    
-    public static Stage stage = new Stage();
-
-    @Override
-    public void start(Stage stage) {
-        
+public class el3ab extends Application {
+	@Override
+	public void start(Stage stage) {
+		
+		String css = this.getClass().getResource("application.css").toExternalForm();
 		Pane root = new Pane();
 		Scene screen=new Scene(root);
-		root.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-        
+		screen.getStylesheets().add(css);
+       
+		
+		StackPane actions= new StackPane();
+		actions.setPrefSize(150,1080);
+		actions.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		root.getChildren().add(actions);
+		
+		/*StackPane stats= new StackPane();
+		stats.setTranslateY(900);
+		stats.setPrefSize(1920,180);
+		stats.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		Image statImg = new Image("greyBack.jpg");
+		BackgroundImage statS = new BackgroundImage(statImg, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(BackgroundSize.AUTO,BackgroundSize.AUTO , false, false, false, true));
+        stats.setBackground(new Background(statS));
+		root.getChildren().add(stats);*/
+		
+		/*StackPane rigth= new StackPane();
+		rigth.setPrefWidth(200);
+		root.getChildren().add(rigth);
+		
+		StackPane left= new StackPane();
+		left.setPrefWidth(200);
+		root.getChildren().add(left);
+		
+		root.getChildren().add(left);*/
+		
+		
+		
+		
 		TilePane board = new TilePane();
-		//board.setTranslateX(200);
-		//board.setTranslateY(70);
-		//Scene a7a = new Scene(board);
-		//board.setPrefSize(1142, 619);
-		board.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		board.setTranslateX(360);
+		board.setStyle("-fx-border-color:BLACK;"+"-fx-border-width: 0 0 0 3;");
+		
 		for(int i=0;i<225;i++) {
-			Button button = new Button();
-			button.setPrefSize(45,45);
-			board.getChildren().add(button);
+			Pane cell = new Pane();
+			cell.setPrefSize(80,72);
+			cell.getStyleClass().add("gridButton");
+			board.getChildren().add(cell);
 		}
-		board.setHgap(1);
-		board.setVgap(1);
 		board.setPrefColumns(15);
 		board.setPrefRows(15);
 		root.getChildren().add(board);
 		
 		
-		
-		
-		
-		
-		/*VBox actions= new VBox();
-		actions.setPrefWidth(120);
-		actions.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-		root.setRight(actions);
-		
-		HBox bottom = new HBox();
-		bottom.setPrefHeight(100);
-		//bottom.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-		root.setBottom(bottom);
-		
-		HBox stats= new HBox();
-		stats.setPrefSize(1142,100);
-		stats.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-		bottom.getChildren().add(stats);
-		
-		HBox exit = new HBox();
-		exit.setPrefSize(120, 100);
-		exit.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-		bottom.getChildren().add(exit);*/
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-        
-		
-		
-       // stage.setWidth(1000);
-       // stage.setHeight(900);
-        //stage.setFullScreen(true);
-        stage.setResizable(false);
+		//Image selectImg = new Image("fog.jpg");
+		//BackgroundImage selectS = new BackgroundImage(selectImg, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, new BackgroundSize(BackgroundSize.AUTO,BackgroundSize.AUTO , false, false, false, true));
+        //board.setBackground(new Background(selectS));
+        stage.setWidth(1600);
+        stage.setHeight(900);
+        stage.setFullScreen(true);
+        stage.setResizable(true);
         stage.setTitle("Game Map");
 		stage.setScene(screen);
         stage.show();
