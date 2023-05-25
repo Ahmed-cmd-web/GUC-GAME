2fcb658ae275d977b4426d802f236f962d2773d7
Abdelrahman <abodykhalil65@gmail.com>
2023-05-25 10:08:17 +0300

    5od

--- GUC-GAME/src/views/el3ab.java
+++ GUC-GAME/src/views/el3ab.java
@@ -1,6 +1,7 @@
 package views;
 
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import jaco.mp3.a.s;
 import javafx.application.*;
@@ -19,22 +20,80 @@ public class el3ab extends Application{
 
     @Override
     public void start(Stage stage) {
-        StackPane root1 = new StackPane();
-		Scene startScreen=new Scene(root1);
         
-
-
-
-        stage.setWidth(1000);
-        stage.setHeight(540);
-        stage.setResizable(false);
+		Pane root = new Pane();
+		Scene screen=new Scene(root);
+		root.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+        
+		TilePane board = new TilePane();
+		//board.setTranslateX(200);
+		//board.setTranslateY(70);
+		//Scene a7a = new Scene(board);
+		//board.setPrefSize(1142, 619);
+		board.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		for(int i=0;i<225;i++) {
+			Button button = new Button();
+			button.setPrefSize(45,45);
+			board.getChildren().add(button);
+		}
+		board.setHgap(1);
+		board.setVgap(1);
+		board.setPrefColumns(15);
+		board.setPrefRows(15);
+		root.getChildren().add(board);
+		
+		
+		
+		
+		
+		
+		/*VBox actions= new VBox();
+		actions.setPrefWidth(120);
+		actions.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		root.setRight(actions);
+		
+		HBox bottom = new HBox();
+		bottom.setPrefHeight(100);
+		//bottom.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		root.setBottom(bottom);
+		
+		HBox stats= new HBox();
+		stats.setPrefSize(1142,100);
+		stats.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		bottom.getChildren().add(stats);
+		
+		HBox exit = new HBox();
+		exit.setPrefSize(120, 100);
+		exit.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+		bottom.getChildren().add(exit);*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+        
+		
+		
+       // stage.setWidth(1000);
+       // stage.setHeight(900);
         //stage.setFullScreen(true);
-        stage.setTitle("Last OF Us");
-		stage.setScene(startScreen);
+        stage.setResizable(false);
+        stage.setTitle("Game Map");
+		stage.setScene(screen);
         stage.show();
         
-    }
-    public static void main(String[] args) {
-        launch(args);
-    }
+	}
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
